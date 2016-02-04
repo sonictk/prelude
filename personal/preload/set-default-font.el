17a51dd1496b3c079f18c64725c3f7835a558441
Siew Yi Liang <y.siew@honteam.com>
2016-02-04 15:12:07 -0500

    - Moved default font setting to use custom personal script

---
+++ personal/preload/set-default-font.el
@@ -0,0 +1,6 @@
+;; Personal custom configuration for Emacs
+;; This is run before Prelude, and is intended to be used with Prelude.
+;; Set custom font as default global font
+(add-to-list 'default-frame-alist '(font . "Literation Mono Powerline-10"))
+(set-face-attribute 'default nil :font "Literation Mono Powerline-10")
+;
