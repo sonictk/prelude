808edbd13d0ea178fe5db5444d9cf7bbf82a293a
Siew Yi Liang <y.siew@honteam.com>
2016-02-04 17:09:01 -0500

    turned on helm globally
    enabled line numbers globally
    
    turned on minimap

--- personal/preload/preload-config.el
+++ personal/preload/preload-config.el
@@ -1,6 +1,12 @@
 ;; Personal custom configuration for Emacs
 ;; This is run before Prelude, and is intended to be used with Prelude.
+
 ;; Set custom font as default global font
 (add-to-list 'default-frame-alist '(font . "Literation Mono Powerline-10"))
 (set-face-attribute 'default nil :font "Literation Mono Powerline-10")
-;
+
+;; This sets the default Emacs theme
+(setq prelude-theme 'monokai)
+
+; Set permanant display of line numbers
+(global-nlinum-mode t)
