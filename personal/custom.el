f9e500036b8dc032590425f44e31160e995bbf8b
Siew Yi Liang <sonictk@gmail.com>
2016-02-05 19:34:33 -0500

    added personal configuration files

---
+++ personal/custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(minimap-always-recenter t)
+ '(minimap-hide-fringes t)
+ '(minimap-minimum-width 10)
+ '(minimap-mode t)
+ '(minimap-width-fraction 0.07)
+ '(minimap-window-location (quote right))
+ '(scroll-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(minimap-font-face ((t (:height 30 :family "Literation Mono for Powerline")))))
