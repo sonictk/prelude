9478636c86d28f5701bfce43fe80c73db97f4089
sonictk <admin@sonictk.com>
2016-02-12 11:08:07 -0500

    - modded config to make scrolling less jumpy

--- personal/gui-config.el
+++ personal/gui-config.el
@@ -103,3 +103,10 @@
 ; Enable autocomplete mode only for C++, Python, and other scripting languages that would make sense
 (global-auto-complete-mode t)
 (add-to-list 'ac-modes 'sql-mode)
+
+;; scroll one line at a time (less "jumpy" than defaults)
+
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq scroll-step 1) ;; keyboard scroll one line at a time
