fd3e5dc7e2c9a68d5fd4b92536547a3fc969aa6d
sonictk <admin@sonictk.com>
2016-03-02 23:54:34 -0800

    - customized tabs display to use different colour

--- personal/gui-config.el
+++ personal/gui-config.el
@@ -41,7 +41,7 @@
   '(
     (space-mark 32 [183] [46]) ; 32 SPACE 「 」, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
     (newline-mark 10 [182 10]) ; 10 LINE FEED
-    (tab-mark 9 [9655 9] [92 9]) ; 9 TAB, 9655 WHITE RIGHT-POINTING TRIANGLE 「▷」
+    (tab-mark 9 [187 9] [92 9]) ; 9 TAB, 9655 WHITE RIGHT-POINTING TRIANGLE 「▷」
     ))
 
 ; Disables the notification sound when scrolling past EOF, among other things
