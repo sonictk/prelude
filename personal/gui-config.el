24a5770aa92d2edd3040b93b460c346ae89f1195
Siew Yi Liang <admin@sonictk.com>
2017-06-29 23:17:59 -0700

    made scrolling more performant

--- personal/gui-config.el
+++ personal/gui-config.el
@@ -333,7 +333,7 @@
 (setq mouse-wheel-scroll-amount '(3 ((shift) . 3))) ;; scroll 3 lines at a time when using mwheel
 (setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
 (setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
-(setq scroll-step 1) ;; keyboard scroll 3 line at a time
+;(setq scroll-step 1) ;; keyboard scroll 3 line at a time
 
 ; Disable guru-mode prompts and tips
 (setq prelude-guru nil)
